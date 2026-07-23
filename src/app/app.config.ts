19c6657e68920412a01f971c3f6b84b43cf71889
@Amin&1378 <aminviper1378@gmail.com>
2026-07-23 11:05:12 +0330

    fix hardcoded api url, use environment config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,40 @@
+// import { Injectable } from '@angular/core';
+// import { environment } from '../environments/environment';
+
+// const hostApi = 'http://localhost';
+// const portApi = '4000';
+// const baseURLApi = `${hostApi}:${portApi}`;
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AppConfig {
+//   config = {
+//     version: '1.2.0',
+//     // remote: 'http://localhost:4000',
+//     isBackend: environment.backend,
+//     hostApi,
+//     portApi,
+//     baseURLApi,
+//   };
+
+//   constructor() {
+//   }
+
+//   getConfig(): Object {
+//     return this.config;
+//   }
+// }
+
+
+
+
+
+
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 
-const hostApi = 'http://localhost';
-const portApi = '4000';
-const baseURLApi = `${hostApi}:${portApi}`;
+const baseURLApi = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +42,7 @@ const baseURLApi = `${hostApi}:${portApi}`;
 export class AppConfig {
   config = {
     version: '1.2.0',
-    // remote: 'http://localhost:4000',
     isBackend: environment.backend,
-    hostApi,
-    portApi,
     baseURLApi,
   };
 
